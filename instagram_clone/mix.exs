6b09510922615ced139b00a5b1d94b6ba1f6f900
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2018-04-23 20:36:36 -0400

    implementing log in

--- instagram_clone/mix.exs
+++ instagram_clone/mix.exs
@@ -32,6 +32,7 @@ defmodule InstagramClone.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.5"},
      {:phoenix_pubsub, "~> 1.0"},
+     {:comeonin, "~> 2.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
--- instagram_clone/web/controllers/user_controller.ex
+++ instagram_clone/web/controllers/user_controller.ex
@@ -1,9 +1,15 @@
 defmodule InstagramClone.UserController do
   use InstagramClone.Web, :controller
   alias InstagramClone.User
+
   def index(conn, _params) do
-    users = Repo.all(InstagramClone.User)
-    render conn, "index.html", users: users
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+      conn ->
+        users = Repo.all(User)
+        render conn, "index.html", users: users
+    end
   end
 
   def show(conn, %{"id" => id}) do
@@ -12,19 +18,31 @@ defmodule InstagramClone.UserController do
   end
 
   def new(conn, _params) do
-   changeset = User.changeset(%User{})
-   render conn, "new.html", changeset: changeset
- end
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
 
- def create(conn, %{"user" => user_params}) do
-   changeset = User.changeset(%User{}, user_params)
-   case Repo.insert(changeset) do
-     {:ok, user} ->
-       conn
-       |> put_flash(:info, "#{user.name} created!")
-       |> redirect(to: user_path(conn, :index))
-     {:error, changeset} ->
-       render(conn, "new.html", changeset: changeset)
-   end
- end
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> InstagramClone.Auth.login(user)
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
